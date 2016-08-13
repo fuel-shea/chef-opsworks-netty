f9372144a60cb4ba7ca15b00cf0542668d878046
Shea Clare <shea@fuelpowered.com>
2016-08-12 17:42:38 -0700

    specify JAVA_HOME

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default["java"]["install_flavor"] = "oracle"
 default["java"]["jdk_version"] = "8"
+default["java"]["java_home"] = "/usr/bin/java"
 default["java"]["oracle"]["accept_oracle_download_terms"] = true
 
 include_attribute "nettyapp::deploy"
